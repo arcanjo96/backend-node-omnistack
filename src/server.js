cfa2498beeb7570c0d4b3e699ea4df80e9e35a61
arcanjo96 <lucasarcanjo.ld@gmail.com>
2019-04-11 15:25:18 -0300

    fix bugs

--- src/server.js
+++ src/server.js
@@ -14,12 +14,7 @@ io.on('connection', socket => {
     });
 });
 
-mongoose.connect("mongodb+srv://admin:luc704605@cluster0-o6huq.mongodb.net/test?retryWrites=true", { useNewUrlParser: true });
-
-app.use((req, res, next) => {
-    req.io = io;
-    return next;
-});
+mongoose.connect("mongodb+srv://admin:1809662@cluster0-o6huq.mongodb.net/omnistack?retryWrites=true", { useNewUrlParser: true });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -27,4 +22,9 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.use(require('./routes'));
 
-app.listen(process.env.PORT || 3050);
+app.use((req, res, next) => {
+    req.io = io;
+    return next;
+});
+
+server.listen(process.env.PORT || 3050);
